e7d63a5cfcf0659c9d7ea68cd84b6368fd8744e4
TAG <taguniversalmachine@gmail.com>
2023-01-07 22:40:48 -0600

    assume yes on add archive

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ apt update
 apt-install --assume-yes make build-essential
 apt install --assume-yes software-properties-common
 apt-get install --assume-yes python3 python3-pip python3-setuptools python3-wheel ninja-build
-add-apt-repository ppa:deadsnakes/ppa
+add-apt-repository --assume-yes ppa:deadsnakes/ppa
 apt update
 apt install --assume-yes python3.8
