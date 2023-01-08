47bbec4582460b9fc871e448409b54d26406834a
TAG <taguniversalmachine@gmail.com>
2023-01-07 22:47:05 -0600

    add device tree compiler

--- setup.sh
+++ setup.sh
@@ -7,3 +7,5 @@ apt-get install --assume-yes python3 python3-pip python3-setuptools python3-whee
 add-apt-repository --assume-yes ppa:deadsnakes/ppa
 apt update
 apt install --assume-yes python3.8
+apt-get install --assume-yes flex bison
+apt install --assume-yes device-tree-compiler
